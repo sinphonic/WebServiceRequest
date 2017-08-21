afada77d5fbf9bb3b3a69fbfa2f19a14708c6a68
sinphonic <chazxlee@gmail.com>
2017-08-21 15:04:19 -0400

    libcurl -- sample request, XML output

--- src/Requestor.cpp
+++ src/Requestor.cpp
@@ -6,11 +6,20 @@
  */
 
 #include<iostream>
+#include<stdlib.h>
+#include<curl/curl.h>
 using namespace std;
 
 int main() {
+	curl_global_init( CURL_GLOBAL_ALL );
+	CURL * handle;
+	CURLcode result;
+	handle = curl_easy_init();
+	curl_easy_setopt(handle, CURLOPT_URL, "https://mesonet.agron.iastate.edu/cgi-bin/wms/iowa/rainfall.cgi?VERSION=1.1.1&REQUEST=GetCapabilities&SERVICE=WMS&");
+	result = curl_easy_perform(handle);
+	curl_easy_cleanup(handle);
 
-	cout << "HELLO!";
+
 	return 0;
 }
 
